d83d38636a15ac13f176ca26185dc9416e374ebc
André Favaro <97260457+andrefavaro@users.noreply.github.com>
2022-01-07 15:40:48 -0300

    Delete Bootcamp-Inter-Dio directory

--- Bootcamp-Inter-Dio/Programando com JS/desafio3.js
+++
@@ -1,9 +0,0 @@
-let lines = gets().split("\n");
-
-let n = parseInt(lines.shift());
-
-let frase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
-
-let fraseResultado = frase.substring(0, n);
-
-console.log(fraseResultado);
